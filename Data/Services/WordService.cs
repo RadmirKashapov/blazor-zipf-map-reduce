0bc79c88818bae677adfef6e0d3c71c3b22d6f3d
RadmirKashapov <radmir.kashapov.150700@gmail.com>
2020-03-30 22:56:47 +0300

    Initial commit with Blazor template

---
+++ Data/Services/WordService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Zipf_Map_Reduce.Data.Services
+{
+    public class WordService
+    {
+    }
+}
---
+++ Pages/Logic/WebMapReduce.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Zipf_Map_Reduce.Pages.Logic
+{
+    public class WebMapReduce
+    {
+    }
+}
